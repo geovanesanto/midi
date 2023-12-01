6bfff92e4767837b9bc8ee56102142917c7e614d
geovanesanto <103213862+geovanesanto@users.noreply.github.com>
2023-12-01 08:40:31 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,18 +1,15 @@
-// Pom
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll ('.tecla');
-
-listaDeTeclas[0].onclick = tocaSomPom;
 
 let contador;
 
 //enquanto
 while(contador < listaDeTeclas.length) [
 
-    listaDeTeclas[contador].onclick = tocaSomPom
+    listaDeTeclas[contador].onclick = tocaSom
 
     contador = contador +1;
 
